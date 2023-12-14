9103d9a4d1a6bb43138c44b624d862473d061a24
Aubin Olivrie <aubin.olivrie@smice.com>
2023-12-14 15:02:34 +0100

    [ADD] Init actions modals

---
+++ src/pages/playlists/editPlaylistModal/index.jsx
@@ -0,0 +1,49 @@
+import {Label, TextInput} from 'flowbite-react';
+import {SpotiModal} from '../../../components/modal';
+import {useEffect, useState} from 'react';
+
+export const EditPlaylistModal = ({show, onClose, playlist}) => {
+  const [newPlaylist, setNewPlaylist] = useState(playlist);
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    setNewPlaylist(playlist);
+  }, [playlist]);
+
+  const handleClose = () => {
+    setNewPlaylist(null);
+    onClose(false);
+  };
+
+  return (
+    <SpotiModal
+      title="Modifier une playlist"
+      confirm="Modifier"
+      show={show}
+      onClose={() => handleClose()}>
+      {!!newPlaylist ? (
+        <div>
+          <Label className="text-spotiblack">Nom de la playlist</Label>
+          <TextInput
+            type="text"
+            placeholder="Nom de la playlist"
+            className="mt-1"
+            value={newPlaylist.name}
+            onChange={e =>
+              setNewPlaylist({...newPlaylist, name: e.target.value})
+            }
+          />
+
+          {/* <SelectMultiple id="countries" options={} /> */}
+
+          {/* 
+          <Table.HeadCell>Musiques</Table.HeadCell>
+          <Table.HeadCell>Image (URL)</Table.HeadCell>
+          <Table.HeadCell>Est un album</Table.HeadCell> */}
+        </div>
+      ) : (
+        <></>
+      )}
+    </SpotiModal>
+  );
+};
