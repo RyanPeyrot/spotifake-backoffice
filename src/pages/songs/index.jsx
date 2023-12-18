c1b47806cdbe4135518c9df4fc767f94e78d9b73
Aubin Olivrie <aubin.olivrie@smice.com>
2023-12-18 22:31:19 +0100

    [ADD] Songs list

--- src/pages/songs/index.jsx
+++ src/pages/songs/index.jsx
@@ -1,26 +1,31 @@
-import {Checkbox, Table} from 'flowbite-react';
+import {Button, Checkbox, Pagination, Table, TextInput} from 'flowbite-react';
 import {useEffect, useState} from 'react';
 import axiosService from '../../services/axios-service';
 import {useToastService} from '../../services/toast-service';
-import {PencilIcon, ArrowTopRightOnSquareIcon} from '@heroicons/react/24/solid';
-import {AddSongModal} from './addSongModal';
+import {ArrowTopRightOnSquareIcon, PencilIcon} from '@heroicons/react/24/solid';
 import {EditSongModal} from './editSongModal';
+import {AddSongModal} from './addSongModal';
+import {DeleteSongModal} from './deleteSongModal';
 
 export const SongsPage = () => {
   const [songs, setSongs] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [filteredSongs, setFilteredSongs] = useState([]);
   const [selectedSongs, setSelectedSongs] = useState([]);
-  const [selectedSong, setSelectedSong] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [addSongModalState, setAddSongModalState] = useState(false);
+  const [selectedSong, setSelectedSong] = useState(null);
   const [editSongModalState, setEditSongModalState] = useState(false);
-
+  const [addSongModalState, setAddSongModalState] = useState(false);
+  const [deleteSongModalState, setDeleteSongModalState] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
   const {addToast} = useToastService();
 
   const handleSelectAll = () => {
     if (selectedSongs.length === songs.length) {
       setSelectedSongs([]);
     } else {
-      setSelectedSongs(songs.map(song => song.id));
+      setSelectedSongs(songs.map(song => song._id));
     }
   };
 
@@ -35,6 +40,31 @@ export const SongsPage = () => {
   const handleEditSong = song => {
     setSelectedSong(song);
     setEditSongModalState(true);
+  };
+
+  const handleEditModalClose = success => {
+    setEditSongModalState(false);
+
+    if (success) {
+      getSongs();
+    }
+  };
+
+  const handleAddModalClose = success => {
+    setAddSongModalState(false);
+
+    if (success) {
+      getSongs();
+    }
+  };
+
+  const handleDeleteModalClose = success => {
+    setDeleteSongModalState(false);
+
+    if (success) {
+      setSelectedSongs([]);
+      getSongs();
+    }
   };
 
   const getSongs = () => {
@@ -52,35 +82,99 @@ export const SongsPage = () => {
         console.error(error);
 
         addToast({
-          type: 'error',
           title: 'Erreur',
-          message:
-            'Une erreur est survenue lors de la récupération des musiques',
+          message: "Impossible d'obtenir la liste des musiques",
+          type: 'error',
         });
       });
   };
 
+  const getAlbums = () => {
+    axiosService
+      .get('/playlists')
+      .then(({data}) => {
+        const onlyAlbums = data
+          .filter(playlist => playlist.isAlbum)
+          .map(album => ({
+            value: album._id,
+            label: album.name,
+          }));
+
+        setAlbums(onlyAlbums);
+      })
+      .catch(error => {
+        console.error(error);
+
+        addToast({
+          title: 'Erreur',
+          message: "Impossible d'obtenir la liste des albums",
+          type: 'error',
+        });
+      });
+  };
+
+  const getArtists = () => {
+    axiosService
+      .get('/artists')
+      .then(({data}) => {
+        const onlyArtists = data.map(artist => ({
+          value: artist._id,
+          label: artist.name,
+        }));
+
+        setArtists(onlyArtists);
+      })
+      .catch(error => {
+        console.error(error);
+
+        addToast({
+          title: 'Erreur',
+          message: "Impossible d'obtenir la liste des artistes",
+          type: 'error',
+        });
+      });
+  };
+
+  const onPageChange = page => setCurrentPage(page);
+
   useEffect(() => {
     getSongs();
-  }, [searchTerm]);
+    getAlbums();
+    getArtists();
+  }, []);
 
-  const filteredSongs = songs.filter(song =>
-    song.tag.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  useEffect(() => {
+    const filteredSongs = songs
+      .filter(song => song.tag.toLowerCase().includes(searchTerm.toLowerCase()))
+      .slice((currentPage - 1) * 10, currentPage * 10);
+
+    setFilteredSongs(filteredSongs);
+  }, [songs, searchTerm, currentPage]);
 
   return (
     <div id="songs">
-      <h1 className="text-2xl mb-5 font-semibold">Liste des musiques</h1>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold">Liste des musiques</h1>
 
-      <input
-        type="text"
-        placeholder="Rechercher une musique (par ID, nom, artiste, album, date de sortie ou référence)"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-        className="w-2/3 p-2 border border-gray-300 text-spotiblack rounded"
-      />
+        <div className="flex gap-4">
+          <TextInput
+            type="text"
+            placeholder="Rechercher une musique"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
 
-      <Table className="mt-6">
+          <Button onClick={() => setAddSongModalState(true)}>Ajouter</Button>
+
+          <Button
+            disabled={selectedSongs.length == 0}
+            onClick={() => setDeleteSongModalState(true)}>
+            Supprimer
+          </Button>
+        </div>
+      </div>
+
+      <Table className="mt-6 min-w-full">
         <Table.Head>
           <Table.HeadCell className="text-center">
             <Checkbox
@@ -89,32 +183,52 @@ export const SongsPage = () => {
           </Table.HeadCell>
           <Table.HeadCell>ID</Table.HeadCell>
           <Table.HeadCell>Nom</Table.HeadCell>
-          <Table.HeadCell>Artistes</Table.HeadCell>
+          <Table.HeadCell>Artiste</Table.HeadCell>
+          <Table.HeadCell>Album</Table.HeadCell>
           <Table.HeadCell>Genre</Table.HeadCell>
           <Table.HeadCell>Ajoutée le</Table.HeadCell>
+          <Table.HeadCell>Image</Table.HeadCell>
           <Table.HeadCell>Lien de la musique</Table.HeadCell>
+          <Table.HeadCell>Éditer</Table.HeadCell>
         </Table.Head>
 
         <Table.Body>
           {filteredSongs.map(song => (
-            <Table.Row key={song.id}>
+            <Table.Row key={song._id}>
               <Table.Cell>
                 <Checkbox
-                  checked={selectedSongs.includes(song.id)}
-                  onChange={() => handleSelect(song.id)}></Checkbox>
+                  checked={selectedSongs.includes(song._id)}
+                  onChange={() => handleSelect(song._id)}></Checkbox>
               </Table.Cell>
               <Table.Cell className="font-semibold">{song._id}</Table.Cell>
-              <Table.Cell>{song.title}</Table.Cell>
+              <Table.Cell>{song.title || '-'}</Table.Cell>
               <Table.Cell>
-                {song.artist.length == 0
+                {song.artist?.length == 0
                   ? '-'
                   : song.artist.map(a => {
                       return a.name;
                     })}
               </Table.Cell>
+              <Table.Cell>{song.album?.name || '-'}</Table.Cell>
               <Table.Cell>{song.genre?.join(', ')}</Table.Cell>
               <Table.Cell>
                 {new Date(song.releaseDate).toLocaleDateString()}
+              </Table.Cell>
+              <Table.Cell>
+                {song.thumbnail ? (
+                  <a
+                    href={song.thumbnail}
+                    target="_blank"
+                    className="text-spotigreen">
+                    <img
+                      className="h-12 w-12"
+                      src={song.thumbnail}
+                      alt="Song thumbnail"
+                    />
+                  </a>
+                ) : (
+                  '-'
+                )}
               </Table.Cell>
               <Table.Cell>
                 <a
@@ -126,7 +240,7 @@ export const SongsPage = () => {
               </Table.Cell>
               <Table.Cell>
                 <PencilIcon
-                  className="w-4 h-4 cursor-pointer"
+                  className="w-4 h-4 cursor-pointer text-spotigreen"
                   onClick={() => handleEditSong(song)}
                 />
               </Table.Cell>
@@ -135,12 +249,38 @@ export const SongsPage = () => {
         </Table.Body>
       </Table>
 
-      <AddSongModal show={addSongModalState} onClose={setAddSongModalState} />
+      {songs.length > 10 && (
+        <div className="flex overflow-x-auto sm:justify-center mt-5">
+          <Pagination
+            currentPage={currentPage}
+            totalPages={Math.ceil(songs.length / 10)}
+            onPageChange={onPageChange}
+            showIcons={true}
+            previousLabel="Précédent"
+            nextLabel="Suivant"
+          />
+        </div>
+      )}
 
       <EditSongModal
         show={editSongModalState}
-        onClose={setEditSongModalState}
+        onClose={success => handleEditModalClose(success)}
         song={selectedSong}
+        albums={albums}
+        artists={artists}
+      />
+
+      <AddSongModal
+        show={addSongModalState}
+        albums={albums}
+        artists={artists}
+        onClose={success => handleAddModalClose(success)}
+      />
+
+      <DeleteSongModal
+        show={deleteSongModalState}
+        songs={selectedSongs}
+        onClose={success => handleDeleteModalClose(success)}
       />
     </div>
   );
